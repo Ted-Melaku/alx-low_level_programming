3da553d6f56f2667a3c17ca21ee30cd7f9863a9e
Ted-Melaku <tedmelaku2024@gmail.com>
2023-03-16 13:20:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry function
- *
- * Retrun: Always 0 when succesful
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
