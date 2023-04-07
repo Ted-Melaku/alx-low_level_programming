b30c28798248c63c0efd78c2005757e340ba62a7
Ted-Melaku <tedmelaku2024@gmail.com>
2023-04-07 05:55:06 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,24 +11,24 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0, num, i, j, k;
+        int result = 0, num, i, j, k;
 
-	for (i = 1; 1 < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-			printf("%s\n", "Error");
-			return (1);
-			}
-		}
-	}
-	for (k = 1; k < argc; k++)
-	{
-		num = atoi(argv[k]);
-		result += num;
-	}
-	printf("%d\n", result);
-	return (0);
+        for (i = 1; 1 < argc; i++)
+        {
+                for (j = 0; argv[i][j] != '\0'; j++)
+                {
+                        if (argv[i][j] > '9' || argv[i][j] < '0')
+                        {
+                        printf("%s\n", "Error");
+                        return (1);
+                        }
+                }
+        }
+        for (k = 1; k < argc; k++)
+        {
+                num = atoi(argv[k]);
+                result += num;
+        }
+        printf("%d\n", result);
+        return (0);
 }
