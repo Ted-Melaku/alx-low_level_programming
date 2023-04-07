15f013ca9fac4648f684cb3e27a57fe4d9d4088c
Ted-Melaku <tedmelaku2024@gmail.com>
2023-04-07 04:17:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function to print its name
+ * @argc: argc parameter
+ * @argv: array of listed commands
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
