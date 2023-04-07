42210697fad251aa07fd147fc605eaa6e4faea99
Ted-Melaku <tedmelaku2024@gmail.com>
2023-04-07 06:50:26 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,13 +6,14 @@
  * main - prints function name
  * @argc: argc parameter
  * @argv: array of a listed command
+ * Return: 0 for success
  */
 
 int main(int argc, char *argv[])
 {
 	int num, j, result = 0;
 
-	int coins[] = {25, 10, 5, 2, 1}
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -36,4 +37,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 	return (0);
 }
-
